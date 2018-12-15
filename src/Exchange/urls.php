64f7e4ba5bdf6ee57d033c4304dbc0f3196a4c6e
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2018-12-15 15:17:59 +0330

    Add API for trades

--- src/Exchange/urls.php
+++ src/Exchange/urls.php
@@ -1,61 +1,14 @@
 <?php
-return array(
-    // ************************************************************* Trades
-    array( // Create
-        'regex' => '#^/trades$#',
-        'model' => 'Exchange_Views_Trade',
-        'method' => 'create',
-        'http-method' => 'POST',
-        'precond' => array(
-            'User_Precondition::loginRequired'
-        )
-    ),
-    array( // Read (list)
-        'regex' => '#^/trades$#',
-        'model' => 'Pluf_Views',
-        'method' => 'findObject',
-        'http-method' => 'GET',
-        'params' => array(
-            'model' => 'Exchange_Trade'
-        )
-    ),
-    array( // Read
-        'regex' => '#^/trades/(?P<modelId>\d+)$#',
-        'model' => 'Pluf_Views',
-        'method' => 'getObject',
-        'http-method' => 'GET',
-        'precond' => array(
-            'User_Precondition::loginRequired'
-        ),
-        'params' => array(
-            'model' => 'Exchange_Trade'
-        )
-    ),
-    array( // Update
-        'regex' => '#^/trades/(?P<modelId>\d+)$#',
-        'model' => 'Pluf_Views',
-        'method' => 'updateObject',
-        'http-method' => 'POST',
-        'params' => array(
-            'model' => 'Exchange_Trade'
-        ),
-        'precond' => array(
-            'User_Precondition::ownerRequired'
-        )
-    ),
-    array( // Delete
-        'regex' => '#^/trades/(?P<modelId>\d+)$#',
-        'model' => 'Pluf_Views',
-        'method' => 'deleteObject',
-        'http-method' => 'DELETE',
-        'precond' => array(
-            'User_Precondition::ownerRequired'
-        ),
-        'params' => array(
-            'model' => 'Exchange_Trade',
-            'permanently' => false
-        )
-    )
+$paths = array_filter(glob(__DIR__ . '/urls/*.php'), function ($file) {
+    return is_file($file);
+});
+
+$sdpApi = array();
+
+foreach ($paths as $path) {
+    $myApi = include $path;
+    $sdpApi = array_merge($sdpApi, $myApi);
+}
+
+return $sdpApi;
     
-    
-);
