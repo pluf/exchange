e9e3b46e378763989edd8f91894ff4160367ee36
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-01-15 21:51:41 +0330

    Improve functionality of comments

--- src/Exchange/Views/Comment.php
+++ src/Exchange/Views/Comment.php
@@ -2,71 +2,57 @@
 Pluf::loadFunction('Pluf_Shortcuts_GetObjectOr404');
 Pluf::loadFunction('Exchange_Shortcuts_NormalizeItemPerPage');
 
-class Exchange_Views_Comment
+class Exchange_Views_Comment extends Pluf_Views
 {
 
-    public static function find($request, $match)
+    public function find($request, $match)
     {
         $parent = Pluf_Shortcuts_GetObjectOr404('Exchange_Offer', $match['parentId']);
-        $comment = new Exchange_Comment();
-        $pag = new Pluf_Paginator($comment);
-        if(User_Precondition::isOwner($request)){            
-            $pag->forced_where = new Pluf_SQL('offer_id=%s', $parent->id);
-        }else{
-            $pag->forced_where = new Pluf_SQL('offer_id=%s AND author_id=%s', array(
-                $parent->id,
-                $request->user->id
-            ));
+        if (!self::canAccessOffer($request, $parent)) {
+            throw new Pluf_Exception_PermissionDenied("Permission is denied");
         }
-        $pag->list_filters = array(
-            'id',
-            'author_id',
-            'offer_id',
-            'mime_type',
-            'media_type'
-        );
-        $search_fields = array(
-            'message'
-        );
-        $sort_fields = array(
-            'id',
-            'author_id',
-            'offer_id',
-            'creation_dtime',
-            'modif_dtime',
-            'mime_type',
-            'media_type'
-        );
-        $pag->items_per_page = Shop_Shortcuts_NormalizeItemPerPage($request);
-        $pag->configure(array(), $search_fields, $sort_fields);
-        $pag->setFromRequest($request);
+        $pag = $this->findManyToOne($request, $match, array(
+            'model' => 'Exchange_Comment',
+            'parent' => 'Exchange_Offer',
+            'parentKey' => 'offer_id'
+        ));
         return $pag;
     }
 
     /**
-     * 
-     * @param Pluf_HTTP_Request $request            
-     * @param array $match            
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
      * @throws Pluf_Exception
      * @return Pluf_HTTP_Response_Json
      */
-    public static function create($request, $match)
+    public function create($request, $match)
     {
-        $parent = Pluf_Shortcuts_GetObjectOr404('Exchange_Offer', $match['parentId']);
-        $object = new Exchange_Comment();
-        $form = Pluf_Shortcuts_GetFormForModel($object, $request->REQUEST);
-        $object = $form->save(false);
-        $object->offer_id = $parent;
+        $object = $this->createManyToOne($request, $match, array(
+            'model' => 'Exchange_Comment',
+            'parent' => 'Exchange_Offer',
+            'parentKey' => 'offer_id'
+        ));
         $object->author_id = $request->user;
-        $object->create();
-        return $object;
+        $object->file_path = Pluf::f('upload_path') . '/' . $request->tenant->id . '/exchange-offer-comment';
+        if (! is_dir($object->file_path)) {
+            if (false == @mkdir($object->file_path, 0777, true)) {
+                throw new Pluf_Form_Invalid('An error occured when creating the upload path. Please try to send the file again.');
+            }
+        }
+        $extra = array(
+            'model' => $object
+        );
+        $form = new Exchange_Form_CommentUpdate(array_merge($request->REQUEST, $request->FILES), $extra);
+        $object->update();
+        return $form->save();
     }
 
-    public static function get($request, $match)
+    public function get($request, $match)
     {
         $parent = Pluf_Shortcuts_GetObjectOr404('Exchange_Offer', $match['parentId']);
         $comment = Pluf_Shortcuts_GetObjectOr404('Exchange_Comment', $match['modelId']);
-        if($comment->offer_id !== $parent->id){
+        if ($comment->offer_id !== $parent->id) {
             throw new Pluf_HTTP_Error404('The Offer has no such Comment.');
         }
         if (self::canAccess($request, $comment))
@@ -76,19 +62,19 @@ class Exchange_Views_Comment
 
     /**
      *
-     * @param Pluf_HTTP_Request $request            
-     * @param array $match            
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
      * @throws Pluf_Exception_PermissionDenied
      * @return Pluf_HTTP_Response_Json
      */
-    public static function update($request, $match)
+    public function update($request, $match)
     {
         $parent = Pluf_Shortcuts_GetObjectOr404('Exchange_Offer', $match['parentId']);
         $comment = Pluf_Shortcuts_GetObjectOr404('Exchange_Comment', $match['modelId']);
-        if($comment->offer_id !== $parent->id){
+        if ($comment->offer_id !== $parent->id) {
             throw new Pluf_HTTP_Error404('The Offer has no such Comment.');
         }
-        if (self::canAccess($request, $comment)){
+        if ($comment->author_id === $request->user->id) {
             $form = Pluf_Shortcuts_GetFormForUpdateModel($comment, $request->REQUEST);
             $updatedComment = $form->save();
             return $updatedComment;
@@ -96,15 +82,14 @@ class Exchange_Views_Comment
         throw new Pluf_Exception_PermissionDenied("Permission is denied");
     }
 
-    public static function delete($request, $match)
+    public function delete($request, $match)
     {
         $parent = Pluf_Shortcuts_GetObjectOr404('Exchange_Offer', $match['parentId']);
         $comment = Pluf_Shortcuts_GetObjectOr404('Exchange_Comment', $match['modelId']);
-        if($comment->offer_id !== $parent->id){
+        if ($comment->offer_id !== $parent->id) {
             throw new Pluf_HTTP_Error404('The Offer has no such Comment.');
         }
-        if (!User_Precondition::isOwner($request) || 
-            $request->user->id === $comment->get_author()->id){
+        if ($request->user->id === $comment->author_id) {
             $comment->delete();
             return $comment;
         }
@@ -113,20 +98,108 @@ class Exchange_Views_Comment
 
     /**
      * Checks if user sending request has access to given Exchange_Comment object
-     * returns true if current user is sender or receiver of comment
-     * 
+     * returns true if current user is offere of the offer which comment is on it
+     * or advertiser of the advertisement which related offer of the comment is on it.
+     *
      * @param Pluf_HTTP_Request $request
      * @param Exchange_Comment $comment
-     * @return boolean 
+     * @return boolean
      */
-    private static function canAccess($request, $comment){
-        if(User_Precondition::isOwner($request)){
+    private static function canAccess($request, $comment)
+    {
+        if (User_Precondition::isOwner($request)) {
             return true;
         }
         $user = $request->user;
-        $sender = $comment->get_sender();
-        $receiver = $comment->get_receiver();
-        // returns true if current user is sender or receiver of comment
-        return (isset($user) && ($user->getId() === $sender->getId() || $user->getId() === $receiver->getId()));
+        $advertiserId = $comment->get_offer()->get_advertisement()->advertiser_id;
+        $offererId = $comment->get_offer()->offerer_id;
+        // returns true if current user is advertiser of advertisement of offer of comment or offerer of offer of the comment
+        return (isset($user) && ($user->getId() === $advertiserId || $user->getId() === $offererId));
     }
+    
+    /**
+     * Checks if user sending request has access to given Exchange_Offer object.
+     * returns true if current user is offerer of the offer or advertiser of the advertisement of the offer.
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param Exchange_Offer $offer
+     * @return boolean
+     */
+    private static function canAccessOffer($request, $offer)
+    {
+        if (User_Precondition::isOwner($request)) {
+            return true;
+        }
+        $user = $request->user;
+        $advertiserId = $offer->get_advertisement()->advertiser_id;
+        $offererId = $offer->offerer_id;
+        // returns true if current user is advertiser of advertisement of offer or offerer of offer
+        return (isset($user) && ($user->getId() === $advertiserId || $user->getId() === $offererId));
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
+     * @return Pluf_HTTP_Response_File
+     */
+    public function download($request, $match)
+    {
+        // GET data
+        if (!array_key_exists('modelId', $match)) {
+            throw new Pluf_Exception_BadRequest('id of comment is not determined');
+        }
+        Pluf::loadFunction('Pluf_Shortcuts_GetObjectOr404');
+        $comment = Pluf_Shortcuts_GetObjectOr404('Exchange_Comment', $match['modelId']);
+        $offer = Pluf_Shortcuts_GetObjectOr404('Exchange_Offer', $match['offerId']);
+        if($offer->id != $comment->get_offer()->id){
+            throw new Pluf_Exception_DoesNotExist('the given comment is not blong to the given order');
+        }
+        $response = new Pluf_HTTP_Response_File($comment->getAbsloutPath(), $comment->mime_type);
+        $response->headers['Content-Disposition'] = sprintf('attachment; filename="%s"', $comment->file_name);
+        return $response;
+    }
+
+    /**
+     * Upload a file as comment
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
+     * @return object
+     */
+    public function updateFile($request, $match)
+    {
+        // TODO: hadi 1397-10-25: prevent update file if comment has file already.
+        $content = Pluf_Shortcuts_GetObjectOr404('Exchange_Comment', $match['modelId']);
+        if (array_key_exists('file', $request->FILES)) {
+            return $this->upload($request, $match);
+        } else {
+            // Do
+            $myfile = fopen($content->getAbsloutPath(), "w") or die("Unable to open file!");
+            $entityBody = file_get_contents('php://input', 'r');
+            fwrite($myfile, $entityBody);
+            fclose($myfile);
+            $content->update();
+        }
+        return $content;
+    }
+    
+    /**
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
+     * @return Exchange_Comment
+     */
+    public static function upload($request, $match)
+    {
+        // تعیین داده‌ها
+        $comment = Pluf_Shortcuts_GetObjectOr404('Exchange_Comment', $match['modelId']);
+        // اجرای درخواست
+        $extra = array(
+            'model' => $comment
+        );
+        $form = new Exchange_Form_CommentUpdate(array_merge($request->REQUEST, $request->FILES), $extra);
+        return $form->save();
+    }
+    
 }
--- src/Exchange/urls/comment.php
+++ src/Exchange/urls/comment.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 return array(
     // ************************************************************* Comments of Offer
     array( // Create
@@ -45,6 +62,27 @@ return array(
         'precond' => array(
             'User_Precondition::loginRequired'
         )
-    )
+    ),
+    // ************************************************************* Binary content of Comment
+    array( // Read
+        'regex' => '#^/offers/(?P<offerId>\d+)/comments/(?P<modelId>\d+)/content$#',
+        'model' => 'Exchange_Views_Comment',
+        'method' => 'download',
+        'http-method' => 'GET',
+        // Cache param
+        'cacheable' => true,
+        'revalidate' => true,
+        'intermediate_cache' => true,
+        'max_age' => 25000
+    ),
+    array( // Update
+        'regex' => '#^/offers/(?P<offerId>\d+)/comments/(?P<modelId>\d+)/content$#',
+        'model' => 'Exchange_Views_Comment',
+        'method' => 'updateFile',
+        'http-method' => 'POST',
+        'precond' => array(
+            'User_Precondition::loginRequired'
+        )
+    ),
 );
 
