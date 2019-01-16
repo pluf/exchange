47bf7e639c076a7374527d0faf66566d647b5958
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-01-16 18:40:05 +0330

    Add API to get an Offer directly

--- src/Exchange/urls/offer.php
+++ src/Exchange/urls/offer.php
@@ -1,5 +1,30 @@
 <?php
 return array(
+    // ************************************************************* Offer
+    array( // Read
+        'regex' => '#^/offers/(?P<modelId>\d+)$#',
+        'model' => 'Pluf_Views',
+        'method' => 'getObject',
+        'http-method' => 'GET',
+        'precond' => array(
+            // TODO: Add precondition to access offer by offerer and advertiser only.
+        ),
+        'params' => array(
+            'model' => 'Exchange_Offer'
+        )
+    ),
+//     array( // Delete
+//         'regex' => '#^/offers/(?P<modelId>\d+)$#',
+//         'model' => 'Pluf_Views',
+//         'method' => 'deleteObject',
+//         'http-method' => 'DELETE',
+//         'precond' => array(
+//             'User_Precondition::ownerRequired'
+//         ),
+//         'params' => array(
+//             'model' => 'Exchange_Offer'
+//         )
+//     ),
     // ************************************************************* Offers of Advertisement
     array( // Create
         'regex' => '#^/advertisements/(?P<parentId>\d+)/offers$#',
